71eca525cf357cd5aba3e585f0f97731a78a81f8
Fedja Bosnic <fedjabosnic@gmail.com>
2017-02-26 11:24:09 +0000

    Added high resolution clock

---
+++ Streaks/Utilities/IClock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streaks.Utilities
+{
+    public interface IClock
+    {
+        /// <summary>
+        /// The current universal date and time.
+        /// </summary>
+        DateTime Time { get; }
+    }
+}
